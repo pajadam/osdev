dd740d5fa5be85a493513bc775245a8d9a38e312
pajadam <pajadam@linux.pl>
2017-06-25 21:32:18 +0200

    Remove raising exception when running bochs returns non-zero value

--- scripts/run.py
+++ scripts/run.py
@@ -4,12 +4,14 @@ import os
 
 
 def run():
+    """ Run generated image in BOCHS """
+
     if not os.path.exists('../conf/osdev.bochsrc'):
         raise Exception('Missing bochs config!')
 
     if not os.path.exists('../bin/floppy.bin'):
-        raise Exception("Missing disk image.\nYou should run 'python manage.py build' first.")
+        raise Exception(
+            "Missing disk image.\nYou should run 'python manage.py build' first.")
 
     print("Running bochs...")
-    if call('bochs -f "../conf/osdev.bochsrc"', shell=True) is not 0:
-        raise Exception("Error running bochs...")
+    call('bochs -f "../conf/osdev.bochsrc"', shell=True)
