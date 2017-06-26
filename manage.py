ea414da586c758d5fc4a46838c7829e59a04b429
Adam Pajkert <pajadam@linux.pl>
2017-06-26 13:50:01 +0200

    Exit with status 1 when invalid arguments are passed

--- manage.py
+++ manage.py
@@ -9,39 +9,36 @@ from scripts import build, run
 
 def main():
 
-    try:
-        # Add missing directories if needed
-        for directory in ("tmp", "bin",):
-            if not os.path.exists(directory):
-                os.makedirs(directory)
+    # Add missing directories if needed
+    for directory in ("tmp", "bin",):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-        # Parse console arguments
-        if len(sys.argv) == 2:
-            os.chdir("tmp/")
-            if sys.argv[1] == "build":
-                build()
-            elif sys.argv[1] == "run":
-                run()
-            elif sys.argv[1] == "test":
-                build()
-                run()
-            elif sys.argv[1] == "clean":
-                os.chdir("..")
-                shutil.rmtree("tmp")
-                print("Cleaned ./tmp directory.")
-            elif sys.argv[1] == "help":
-                print("Usage:")
-                print("  build\t- Build project")
-                print("  run\t- Run project")
-                print("  test\t- Build and run project")
-                print("  clean\t- Clean temporary directory\n")
-            else:
-                print("Invalid argument: " + sys.argv[1])
+    # Parse console arguments
+    if len(sys.argv) == 2:
+        os.chdir("tmp/")
+        if sys.argv[1] == "build":
+            build()
+        elif sys.argv[1] == "run":
+            run()
+        elif sys.argv[1] == "test":
+            build()
+            run()
+        elif sys.argv[1] == "clean":
+            os.chdir("..")
+            shutil.rmtree("tmp")
+            print("Cleaned ./tmp directory.")
+        elif sys.argv[1] == "help":
+            print("Usage:")
+            print("  build\t- Build project")
+            print("  run\t- Run project")
+            print("  test\t- Build and run project")
+            print("  clean\t- Clean temporary directory\n")
         else:
-            print("Missing arguments. Use 'help' to display usage.")
-
-    except:
-        print(traceback.format_exc())
+            print("Invalid argument: " + sys.argv[1])
+            sys.exit(1)
+    else:
+        print("Missing arguments. Use 'help' to display usage.")
         sys.exit(1)
 
 if __name__ == "__main__":
